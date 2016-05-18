a66feba18e8d8d4d1be3bf6afc9d74e93df3c953
Jo Kerkhofs <jke@thinkerit.be>
2016-05-18 12:40:45 +0200

    Format ngModel with custom time-formatter
    To be used for gracefull fallback without displaying seconds and milliseconds.

--- src/momentx.js
+++ src/momentx.js
@@ -1,1 +1,13 @@
-angular.module('momentx', ['angularMoment']);
+angular.module('momentx', ['angularMoment'])
+    .directive('binTimeFormatter', ['moment', function (moment) {
+        return {
+            require: '?ngModel',
+            link: function (scope, el, attrs, ngModel) {
+                if(!ngModel || attrs.type != 'time' || !attrs.timeFormat) return;
+
+                ngModel.$formatters.unshift(function(value) {
+                    if (value.match(/^\d\d:\d\d:\d\d\.\d\d\d$/)) return moment(value, 'HH:mm:ss.sss').format(attrs.timeFormat);
+                });
+            }
+        }
+    }]);
